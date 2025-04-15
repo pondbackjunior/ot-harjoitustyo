e18829f547412c91d0fe172bab2d7ccb8e6c18eb
Ville Lampiselkä <148494501+pondbackjunior@users.noreply.github.com>
2025-04-15 23:00:58 +0300

    autopep cleanup

--- src/editors/main.py
+++ src/editors/main.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 
+
 class EditorMain:
     def __init__(self, root, textarea):
         self._root = root
         self._textarea = textarea
 
         # Keyboard shortcuts
-        self._textarea.bind("<Control-b>", lambda _: (self.insert_tag("b"), "break")[1]) # Bold
-        self._textarea.bind("<Control-i>", lambda _: (self.insert_tag("i"), "break")[1]) # Italic
-        self._textarea.bind("<Control-u>", lambda _: (self.insert_tag("u"), "break")[1]) # Underline
-        self._textarea.bind("<Control-a>", lambda _: (self._textarea.tag_add("sel", "1.0", "end-1c"), "break")[1]) # Select all
-        self._textarea.bind("<Control-z>", self.undo) # Undo
-        self._textarea.bind("<Control-Shift-Z>", self.redo) # Redo
+        self._textarea.bind(
+            "<Control-b>", lambda _: (self.insert_tag("b"), "break")[1])  # Bold
+        self._textarea.bind(
+            "<Control-i>", lambda _: (self.insert_tag("i"), "break")[1])  # Italic
+        self._textarea.bind(
+            "<Control-u>", lambda _: (self.insert_tag("u"), "break")[1])  # Underline
+        self._textarea.bind("<Control-a>", lambda _: (
+            self._textarea.tag_add("sel", "1.0", "end-1c"), "break")[1])  # Select all
+        self._textarea.bind("<Control-z>", self.undo)  # Undo
+        self._textarea.bind("<Control-Shift-Z>", self.redo)  # Redo
 
     def insert_tag(self, tag):
         """
@@ -36,17 +41,16 @@ class EditorMain:
             self._textarea.mark_set("insert", cursor_index)
 
     # Pylint's disable unused-argument is used because the event argument is required (otherwise it runs into an error), but we don't use it.
-    def undo(self, event=None): #pylint: disable=unused-argument
+    def undo(self, event=None):  # pylint: disable=unused-argument
         try:
             self._textarea.edit_undo()
         except tk.TclError:
             pass
         return "break"
 
-    def redo(self, event=None): #pylint: disable=unused-argument
+    def redo(self, event=None):  # pylint: disable=unused-argument
         try:
             self._textarea.edit_redo()
         except tk.TclError:
             pass
         return "break"
-
